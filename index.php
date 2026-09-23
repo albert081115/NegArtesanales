ad0cf335b677d6b11bb449aef4336bc26ca31766
Gerardo <l23070005@huetamo.tecnm.mx>
2026-09-23 08:01:10 -0600

    Cree el apartado de estilos y codigo

--- index.php
+++ index.php
@@ -6,6 +6,8 @@
     <title>Catálogo - Plataforma Artesanal</title>
     <!-- Conexión a Bootstrap 5 (CSS) -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    
+    <link rel="stylesheet" href="estilos.css">
 </head>
 <body class="bg-light">
 
